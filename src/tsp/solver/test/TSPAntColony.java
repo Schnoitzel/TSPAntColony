632839582a1522df694f60a4d1ef38fa52ab744f
ioannis <ioannis.charalampakis@haw-hamburg.de>
2024-05-23 18:17:04 +0200

    dopplungen gelöscht und anzeige aktualisiert

--- src/tsp/solver/test/TSPAntColony.java
+++ src/tsp/solver/test/TSPAntColony.java
@@ -1,4 +1,5 @@
 package tsp.solver.test;
+
 import java.util.*;
 
 import org.graphstream.graph.implementations.SingleGraph;
@@ -11,11 +12,11 @@ import org.graphstream.algorithm.Dijkstra;
 
 public class TSPAntColony {
 
-    private static final int NODES = 100;
+    private static final int NODES = 10;
     private static final double INITIAL_PHEROMONE = 1.0;
     private static final double EVAPORATION_RATE = 0.5;
-    private static final double Q = 1.0;
-    private static final double ALPHA = 1.0;
+    static double Q = 1.0;
+    static double ALPHA = 1.0;
 
     private static double[][] distances = new double[NODES][NODES];
     private static double[][] pheromones = new double[NODES][NODES];
@@ -29,35 +30,20 @@ public class TSPAntColony {
             tours = simulateAnts();
             updatePheromones(tours);
             evaporatePheromones();
-           // visualizeShortestPath();
         }
 
         List<Integer> bestTour = findBestTour(tours);
         int bestTourCost = calculateTourCost(bestTour);
         visualizeGraph();
+        visualizeBestTourInGraph(bestTour);
         visualizeBestTour(bestTour);
-
-        printDistancesAndPheromones();
+       // printDistancesAndPheromones();
         System.out.println("Beste Tour: " + bestTour);
         System.out.println("Gesamtkosten: " + bestTourCost);
-    }
-    private static void printDistancesAndPheromones() {
-        for (int i = 0; i < NODES; i++) {
-            for (int j = 0; j < NODES; j++) {
-                System.out.printf("%8.2f", distances[i][j]); // Formatiere die Ausgabe auf zwei Dezimalstellen
-            }
-            System.out.println(); // Neue Zeile am Ende jeder Zeile des Arrays
-        }
-        System.out.println();
-        for (int i = 0; i < NODES; i++) {
-            for (int j = 0; j < NODES; j++) {
-                System.out.printf("%8.2f", pheromones[i][j]); // Formatiere die Ausgabe auf zwei Dezimalstellen
-            }
-            System.out.println(); // Neue Zeile am Ende jeder Zeile des Arrays
-        }
 
     }
-    private static void initializeDistances() {
+
+    static void initializeDistances() {
         Random random = new Random();
         for (int i = 0; i < NODES; i++) {
             for (int j = 0; j < NODES; j++) {
@@ -69,16 +55,16 @@ public class TSPAntColony {
                 }
             }
         }
-       // System.out.println("distances: " + distances);
+        // System.out.println("distances: " + distances);
     }
 
-    private static void initializePheromones() {
+    static void initializePheromones() {
         for (int i = 0; i < NODES; i++) {
             Arrays.fill(pheromones[i], INITIAL_PHEROMONE);
         }
     }
 
-    private static List<List<Integer>> simulateAnts() {
+    static List<List<Integer>> simulateAnts() {
         List<List<Integer>> tours = new ArrayList<>();
 
         for (int ant = 0; ant < NODES; ant++) {
@@ -90,12 +76,10 @@ public class TSPAntColony {
                 int nextNode = chooseNextNode(currentNode, tour);
                 tour.add(nextNode);
             }
-
             tour.add(tour.get(0));
             tours.add(tour);
             depositPheromones(tour);
         }
-
         return tours;
     }
 
@@ -117,11 +101,9 @@ public class TSPAntColony {
                 totalProbability += probabilities[nextNode];
             }
         }
-
         for (int i = 0; i < probabilities.length; i++) {
             probabilities[i] /= totalProbability;
         }
-
         return probabilities;
     }
 
@@ -141,7 +123,7 @@ public class TSPAntColony {
         return -1; // Sollte nie erreicht werden
     }
 
-    private static void updatePheromones(List<List<Integer>> tours) {
+    static void updatePheromones(List<List<Integer>> tours) {
         for (List<Integer> tour : tours) {
             int tourCost = calculateTourCost(tour);
 
@@ -154,7 +136,7 @@ public class TSPAntColony {
         }
     }
 
-    private static void evaporatePheromones() {
+    static void evaporatePheromones() {
         for (int i = 0; i < NODES; i++) {
             for (int j = 0; j < NODES; j++) {
                 pheromones[i][j] *= (1 - EVAPORATION_RATE);
@@ -173,7 +155,7 @@ public class TSPAntColony {
         }
     }
 
-    private static int calculateTourCost(List<Integer> tour) {
+    static int calculateTourCost(List<Integer> tour) {
         int totalCost = 0;
 
         for (int i = 0; i < tour.size() - 1; i++) {
@@ -186,7 +168,7 @@ public class TSPAntColony {
         return totalCost;
     }
 
-    private static List<Integer> findBestTour(List<List<Integer>> tours) {
+    static List<Integer> findBestTour(List<List<Integer>> tours) {
         int bestTourCost = Integer.MAX_VALUE;
         List<Integer> bestTour = null;
 
@@ -197,7 +179,6 @@ public class TSPAntColony {
                 bestTour = tour;
             }
         }
-
         return bestTour;
     }
 
@@ -229,151 +210,10 @@ public class TSPAntColony {
                 "node { text-size: 20px; text-color: black; fill-color: red; }" +
                         "edge { text-size: 15px; text-color: blue; }"
         );
-
-        // Graph anzeigen
-        graph.display();
-
-    }
-        private static void visualizeGraph1() {
-        System.setProperty("org.graphstream.ui", "swing");
-
-        Graph graph = new SingleGraph("Graph");
-
-        // Knoten hinzufügen
-        for (int i = 0; i < NODES; i++) {
-            graph.addNode(String.valueOf(i));
-        }
-
-        // Kanten hinzufügen
-        for (int i = 0; i < NODES; i++) {
-            for (int j = 0; j < NODES; j++) {
-                if (i != j && distances[i][j] > 0) { // Keine Schleifen und nur positive Distanzen
-                    String edgeId = i + "-" + j;
-                    Edge edge = graph.addEdge(edgeId, String.valueOf(i), String.valueOf(j), true);
-                    edge.setAttribute("ui.label", String.format("%.2f", distances[i][j]));
-                }
-            }
-        }
-
-        // Styling für den Graphen
-        graph.setAttribute("ui.stylesheet",
-                "node { text-size: 40px; text-color: yellow; fill-color: red; }" +
-                        "edge { text-size: 25px; text-color: blue; }"
-        );
-
-        // Labels anzeigen
-        for (Node node : graph) {
-            node.setAttribute("ui.label", node.getId());
-        }
-
         // Graph anzeigen
         graph.display();
     }
-    private static void visualizeShortestPath1() {
-        System.setProperty("org.graphstream.ui", "swing");
 
-        Graph graph = new SingleGraph("Graph");
-
-        // Knoten hinzufügen
-        for (int i = 0; i < NODES; i++) {
-            graph.addNode(String.valueOf(i));
-        }
-
-        // Kanten hinzufügen
-        for (int i = 0; i < NODES; i++) {
-            for (int j = 0; j < NODES; j++) {
-                if (i != j && distances[i][j] > 0) { // Keine Schleifen und nur positive Distanzen
-                    String edgeId = i + "-" + j;
-                    Edge edge = graph.addEdge(edgeId, String.valueOf(i), String.valueOf(j), true);
-                    edge.setAttribute("weight", distances[i][j]);
-                }
-            }
-        }
-
-        // Dijkstra-Algorithmus anwenden, um den kürzesten Pfad zu finden
-        Dijkstra dijkstra = new Dijkstra(Dijkstra.Element.EDGE, null, "weight");
-        dijkstra.init(graph);
-        dijkstra.setSource(graph.getNode("0")); // Startknoten
-        dijkstra.compute();
-
-        // Kürzesten Pfad von Startknoten zu allen anderen Knoten finden
-        for (Node node : graph) {
-            if (dijkstra.getPathLength(node) < Double.POSITIVE_INFINITY) {
-                for (Edge edge : dijkstra.getPathEdges(node)) {
-                    edge.setAttribute("ui.style", "fill-color: red;");
-                }
-            }
-        }
-
-        // Styling für den Graphen
-        graph.setAttribute("ui.stylesheet",
-                "node { text-size: 20px; text-color: black; fill-color: red; }" +
-                        "edge { text-size: 15px; text-color: blue; }"
-        );
-
-        // Labels anzeigen
-        for (Node node : graph) {
-            node.setAttribute("ui.label", node.getId());
-        }
-
-        // Graph anzeigen
-        graph.display();
-    }
-    private static void visualizeShortestPath() {
-        System.setProperty("org.graphstream.ui", "swing");
-
-        Graph graph = new SingleGraph("Graph");
-
-        // Knoten hinzufügen
-        for (int i = 0; i < NODES; i++) {
-            graph.addNode(String.valueOf(i));
-        }
-
-        // Kanten hinzufügen
-        for (int i = 0; i < NODES; i++) {
-            for (int j = 0; j < NODES; j++) {
-                if (i != j && distances[i][j] > 0) { // Keine Schleifen und nur positive Distanzen
-                    String edgeId = i + "-" + j;
-                    Edge edge = graph.addEdge(edgeId, String.valueOf(i), String.valueOf(j), true);
-                    edge.setAttribute("weight", distances[i][j]);
-                }
-            }
-        }
-
-        // Dijkstra-Algorithmus anwenden, um den kürzesten Pfad zu finden
-        Dijkstra dijkstra = new Dijkstra(Dijkstra.Element.EDGE, null, "weight");
-        dijkstra.init(graph);
-        dijkstra.setSource(graph.getNode("0")); // Startknoten
-        dijkstra.compute();
-
-        // Erstellen eines neuen Graphen nur mit dem kürzesten Pfad
-        Graph shortestPathGraph = new SingleGraph("ShortestPathGraph");
-
-        // Knoten des kürzesten Pfades hinzufügen
-        for (Node node : dijkstra.getPathNodes(graph.getNode(String.valueOf(NODES - 1)))) {
-            shortestPathGraph.addNode(node.getId());
-            Node newNode = shortestPathGraph.getNode(node.getId());
-            newNode.setAttribute("ui.label", newNode.getId());
-        }
-
-        // Kanten des kürzesten Pfades hinzufügen
-        for (Edge edge : dijkstra.getPathEdges(graph.getNode(String.valueOf(NODES - 1)))) {
-            String source = edge.getSourceNode().getId();
-            String target = edge.getTargetNode().getId();
-            String edgeId = source + "-" + target;
-            Edge newEdge = shortestPathGraph.addEdge(edgeId, source, target, true);
-            newEdge.setAttribute("ui.label", String.format("%.2f", edge.getAttribute("weight")));
-        }
-
-        // Styling für den Graphen
-        shortestPathGraph.setAttribute("ui.stylesheet",
-                "node { text-size: 20px; text-color: black; fill-color: red; }" +
-                        "edge { text-size: 15px; text-color: blue; }"
-        );
-
-        // Graph anzeigen
-        shortestPathGraph.display();
-    }
 
 
     private static void visualizeBestTour(List<Integer> bestTour) {
@@ -404,5 +244,71 @@ public class TSPAntColony {
 
         // Graph anzeigen
         graph.display();
+    }
+
+    private static void visualizeBestTourInGraph(List<Integer> bestTour) {
+        System.setProperty("org.graphstream.ui", "swing");
+
+        Graph graph = new SingleGraph("Graph");
+
+        // Knoten hinzufügen
+        for (int i = 0; i < NODES; i++) {
+            graph.addNode(String.valueOf(i));
+        }
+
+        // Kanten hinzufügen
+        for (int i = 0; i < NODES; i++) {
+            for (int j = 0; j < NODES; j++) {
+                if (i != j && distances[i][j] > 0) { // Keine Schleifen und nur positive Distanzen
+                    String edgeId = i + "-" + j;
+                    Edge edge = graph.addEdge(edgeId, String.valueOf(i), String.valueOf(j), true);
+                    edge.setAttribute("weight", distances[i][j]);
+                }
+            }
+        }
+
+        // Kanten der besten Rundreisewegs markieren
+        for (int i = 0; i < bestTour.size() - 1; i++) {
+            int source = bestTour.get(i);
+            int target = bestTour.get(i + 1);
+            String edgeId = source + "-" + target;
+            Edge edge = graph.getEdge(edgeId);
+            edge.setAttribute("ui.style", "fill-color: green;");
+            edge.setAttribute("ui.label", String.format("%.2f", edge.getAttribute("weight")));
+        }
+
+
+
+        // Styling für den Graphen
+        graph.setAttribute("ui.stylesheet",
+                "node { text-size: 20px; text-color: black; fill-color: red; }" +
+                        "edge { text-size: 15px; text-color: blue; }"
+        );
+
+        // Labels anzeigen
+        for (Node node : graph) {
+            node.setAttribute("ui.label", node.getId());
+        }
+
+        // Graph anzeigen
+        graph.display();
+    }
+
+
+    private static void printDistancesAndPheromones() {
+        for (int i = 0; i < NODES; i++) {
+            for (int j = 0; j < NODES; j++) {
+                System.out.printf("%8.2f", distances[i][j]); // Formatiere die Ausgabe auf zwei Dezimalstellen
+            }
+            System.out.println(); // Neue Zeile am Ende jeder Zeile des Arrays
+        }
+        System.out.println();
+        for (int i = 0; i < NODES; i++) {
+            for (int j = 0; j < NODES; j++) {
+                System.out.printf("%8.2f", pheromones[i][j]); // Formatiere die Ausgabe auf zwei Dezimalstellen
+            }
+            System.out.println(); // Neue Zeile am Ende jeder Zeile des Arrays
+        }
+
     }
 }
